97d92c0a4d6493176ecd14b6e45cba925f805db0
Patwari Raghottam <65483520+ragh945@users.noreply.github.com>
2024-07-27 20:24:15 +0530

    Add files via upload

---
+++ Email_spam.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from PIL import Image
+inno=Image.open(r"C:\Users\RAGHAVENDRA KUMAR\OneDrive\Pictures\Screenshots\Inno.png")
+st.image(inno, use_column_width=True)
+email_logo=Image.open(r"C:\Users\RAGHAVENDRA KUMAR\Downloads\email.jpg")
+st.image(email_logo, caption='Email', use_column_width=False)
+# Load the pre-trained model and vectorizer
+model_path = r"C:\Users\RAGHAVENDRA KUMAR\ML\NLP\Email_spam_ham.pkl"
+vectorizer_path = r"C:\Users\RAGHAVENDRA KUMAR\ML\NLP\tfidf_vectorization.pkl"
+
+with open(model_path, 'rb') as model_file:
+    model = pickle.load(model_file)
+
+with open(vectorizer_path, 'rb') as vectorizer_file:
+    tfidf = pickle.load(vectorizer_file)
+
+st.title("Email Spam Classification")
+
+
+# Input email from user
+email = st.text_input("Enter the email: ")
+
+# Transform the input email using the loaded vectorizer
+if st.button("Submit"):
+    data = tfidf.transform([email]).toarray()
+    pred = model.predict(data)[0]
+    
+    if pred == 'spam':
+        st.write("The Email is: Spam")
+        spam_image = Image.open(r"C:\Users\RAGHAVENDRA KUMAR\Downloads\spam.jpg")
+        st.image(spam_image, caption='Spam Email', use_column_width=False)
+    else:
+        st.write("The Email is: Ham")
+        ham_image = Image.open(r"C:\Users\RAGHAVENDRA KUMAR\Downloads\ham.jpg")
+        st.image(ham_image, caption='Ham Email', use_column_width=False)
